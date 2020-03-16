2f22df60506996a7befa17ea7531f499d2449fa5
Tony Tang <teokigi@hotmail.com>
2020-03-16 21:32:37 +0000

    created test for getting book titles

--- library_class.rb
+++ library_class.rb
@@ -8,6 +8,15 @@ attr_writer :books, :library
 
     end
 
+    def get_books
+        return @books.title
+    end
+#create a getter for the books
+#create a method that takes in a book title and retuns all assosiated information
+#create a method that takes in a book title and adds it to book list
+#student names and date are empty strings
+#create a method that changes the rental details by taking in the title of the book,
+#the student renting it and the date its due to be returned
 
 
 end
--- specs/library_class_specs.rb
+++ specs/library_class_specs.rb
@@ -6,8 +6,8 @@ require_relative('../library_class.rb')
 class TestLibraryClass < MiniTest::Test
 
     def setup
-        tokyo_library = LibraryClass.new("tokyo_city_library")
-        tokyo_library.books << {title: "lord_of_the_rings",
+        @tokyo_library = LibraryClass.new("tokyo_city_library")
+        @tokyo_library.books << {title: "lord_of_the_rings",
                                 rental_details:{
                                     student_name: "jeff",
                                     date: "01/12/16"
@@ -15,4 +15,14 @@ class TestLibraryClass < MiniTest::Test
                                 }
     end
 
+    #create a test getter for the books
+    def test_001_get_books
+        p @tokyo_library.get_books
+        assert_equal("lord_of_the_rings",@toky_library.get_books)
+    end
+    #create a test for method that takes in a book title and retuns all assosiated information
+    #create a test for method that takes in a book title and adds it to book list
+    #student names and date are empty strings
+    #create a test for method that changes the rental details by taking in the title of the book,
+    #the student renting it and the date its due to be returned
 end
