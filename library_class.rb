38db7d8c5cd6337d54b45c4a610d8fd2ded5976e
Tony Tang <teokigi@hotmail.com>
2020-03-16 21:12:50 +0000

    created class attributes

--- library_class.rb
+++ library_class.rb
@@ -1,7 +1,17 @@
 class LibraryClass
 
-    def initialize
+    def initialize(library_name)
+        @library = library_name
+        @books = {
+                title: "null",
+                rental_details:{
+                    student_name: "null",
+                    date:"null"
+                }
+        }
 
     end
+
+    
 
 end
